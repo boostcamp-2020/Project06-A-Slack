c674d289b7188c6f8aa1c522fc9bdcde02d89612
rnjshippo <tkstka0023@naver.com>
2020-11-18 21:05:16 +0900

    [feat] 소켓 기본 연결 코드 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import { Header } from '@components/index';
+import io from 'socket.io-client';
 
-const App = () => (
-  <div>
-    <Header />
-  </div>
-);
+const domain = 'http://localhost:3000';
+
+const App = () => {
+  const socket1 = io(`${domain}/channel1`, {
+    transports: ['websocket', 'polling'],
+  });
+  const socket2 = io(`${domain}/channel2`, {
+    transports: ['websocket', 'polling'],
+  });
+
+  const handleSend1 = () => {
+    console.log('ch1 clicked');
+    socket1.emit('msg', 'value');
+  };
+
+  const handleSend2 = () => {
+    console.log('ch2 clicked');
+    socket2.emit('msg', 'value');
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleSend1}>
+        채널 1 전송
+      </button>
+      <button type="button" onClick={handleSend2}>
+        채널 2 전송
+      </button>
+      <Header />
+    </div>
+  );
+};
 
 export default App;
--- server/src/app.ts
+++ server/src/app.ts
@@ -7,9 +7,42 @@ import cookieParser from 'cookie-parser';
 import createError from 'http-errors';
 import cors from 'cors';
 import { Error } from '@lib/types';
+import SocketIO, { Socket } from 'socket.io';
+import http from 'http';
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIO.Server(server, { transports: ['websocket', 'polling'] });
+
+const channel1 = io.of('/channel1');
+const channel2 = io.of('/channel2');
+
+channel1.on('connection', (socket: Socket) => {
+  console.log('채널 1 연결된 socketID : ', socket.id);
+  io.to(socket.id).emit('my socket id', { socketId: socket.id });
+
+  socket.on('msg', (data) => {
+    console.log('channel 1 메시지', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('연결 끊김, 바이');
+  });
+});
+
+channel2.on('connection', (socket: Socket) => {
+  console.log('채널 2 연결된 socketID : ', socket.id);
+  io.to(socket.id).emit('my socket id', { socketId: socket.id });
+
+  socket.on('msg', (data) => {
+    console.log('channel 2 메시지', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('연결 끊김, 바이');
+  });
+});
 
 app.use(cors());
 app.use(logger('dev'));
@@ -34,6 +67,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.json({ err });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
