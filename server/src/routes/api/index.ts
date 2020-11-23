86961ef54df4e968396ae3fd7086c72e47b8b523
rnjshippo <tkstka0023@naver.com>
2020-11-23 21:34:17 +0900

    [feat] auth 미들웨어 적용

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { authenticated } from '@/middlewares/auth.middleware';
 import userRouter from './users';
 import channelRouter from './channels';
 import authRouter from './auth';
@@ -6,9 +7,9 @@ import threadRouter from './threads';
 
 const router = express.Router();
 
-router.use('/users', userRouter);
+router.use('/users', authenticated, userRouter);
 router.use('/channels', channelRouter);
-router.use('/auth', authRouter);
-router.use('/threads', threadRouter);
+router.use('/auth', authenticated, authRouter);
+router.use('/threads', authenticated, threadRouter);
 
 export default router;
