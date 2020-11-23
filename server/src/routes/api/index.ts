f33a1102a23b4cc028e1e4242eff4ca97056192c
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-23 13:19:10 +0900

    feat: channel api에 대한 mock서버를 구현

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import userRouter from './users';
+import channelRouter from './channels';
 
 const router = express.Router();
 
 router.use('/users', userRouter);
-
+router.use('/channels', channelRouter);
 export default router;
