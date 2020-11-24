2b1e333e2af40b47af764d9e226ee9f8db8506e8
rnjshippo <tkstka0023@naver.com>
2020-11-25 02:12:50 +0900

    [refactor] store 생성, 사가 연결 부분을 store/index.ts로 분리

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configureStore } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
 import App from '@/App';
-import rootReducer from '@/store/modules';
-import sagas from '@/store/sagas';
-
-const sagaMiddleware = createSagaMiddleware();
-
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: [sagaMiddleware],
-  devTools: process.env.MODE === 'dev',
-});
-
-sagaMiddleware.run(sagas);
+import store from '@/store';
 
 const rootElement = document.getElementById('root');
 
