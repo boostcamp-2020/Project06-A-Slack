0baeeb1dcf0ad8eed7e27ed902a4ff7395ecbf4c
rnjshippo <tkstka0023@naver.com>
2020-11-25 02:28:44 +0900

    [feat] css nomalize 적용 - GlobalStyle
    styled-components의 GlobalStyle 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from '@/App';
 import store from '@/store';
+import { GlobalStyle } from '@/styles';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>,
   rootElement,
