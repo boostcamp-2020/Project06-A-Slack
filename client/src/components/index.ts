87ccb066cc707ad1290334ec11d8592e886e6945
grap3fruit <swkwon333@gmail.com>
2020-11-25 22:46:08 +0900

    [feat] index에 thread 추가

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './common';
 export { default as LoginBox } from './LoginBox/LoginBox';
 export { default as ChannelListBox } from './ChannelListBox/ChannelListBox';
+export { default as ThreadListBox } from './ThreadListBox/ThreadListBox';
--- client/src/store/sagas/index.ts
+++ client/src/store/sagas/index.ts
@@ -1,7 +1,8 @@
 import { all, fork } from 'redux-saga/effects';
 import authSaga from './authSaga';
 import channelSaga from './channelSaga';
+import threadSaga from './threadSaga';
 
 export default function* rootSaga() {
-  yield all([fork(authSaga), fork(channelSaga)]);
+  yield all([fork(authSaga), fork(channelSaga), fork(threadSaga)]);
 }
