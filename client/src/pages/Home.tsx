fc2a44a536742d3898aec104217cebfdd3903e75
rnjshippo <tkstka0023@naver.com>
2020-11-25 23:41:45 +0900

    [feat] 커스텀 훅 적용

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectAuth, AUTH_ACTIONS } from '@/store/modules/auth';
+import { useDispatch } from 'react-redux';
+import { AUTH_ACTIONS } from '@/store/modules/auth';
 import { ChannelListBox } from '@/components';
+import { useAuth } from '@/hooks';
 
 const Home = () => {
   const dispatch = useDispatch();
 
-  const { user, isLogin } = useSelector(selectAuth);
+  const { accessToken } = useAuth();
 
   const handleLogout = () => {
     dispatch(AUTH_ACTIONS.logoutRequest());
@@ -16,7 +17,7 @@ const Home = () => {
   return (
     <div>
       <p>Home page</p>
-      {isLogin ? (
+      {accessToken ? (
         <>
           <ChannelListBox />
           <button type="button" onClick={handleLogout}>
