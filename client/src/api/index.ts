99a09a7937749fcc6d6b88c5e6f364ac454e770d
rnjshippo <tkstka0023@naver.com>
2020-11-25 19:42:00 +0900

    [feat] axios 커스텀 - 토큰 헤더에 추가 및 재 요청

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ERROR_MESSAGE } from '@/utils/constants';
 
 const instance = axios.create({ timeout: 8000 });
 
@@ -6,14 +7,39 @@ if (process.env.MODE !== 'dev') {
   instance.defaults.baseURL = process.env.BASE_URL;
 }
 
-// instance.interceptors.request.use(
-//   (config) => {
-//     const accessToken = localStorage.getItem('accessToken');
-//     return { ...config, headers: { ...config.headers, Authorization: `Bearer ${accessToken}` } };
-//   },
-//   (err) => {
-//     return Promise.reject(err);
-//   },
-// );
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    return { ...config, headers: { ...config.headers, Authorization: `Bearer ${accessToken}` } };
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
+
+instance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    const originalReq = err.config;
+    if (err.response.status === 401 && err.response.data?.message === ERROR_MESSAGE.INVALID_TOKEN) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (refreshToken) {
+        const { data, status } = await axios.post('/api/auth/token/refresh', { refreshToken });
+        if (status === 200) {
+          const { accessToken } = data;
+          localStorage.setItem('accessToken', accessToken);
+          return instance.request(originalReq);
+        }
+        if (status === 401) {
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('refreshToken');
+        }
+      }
+    }
+    return Promise.reject(err);
+  },
+);
 
 export default instance;
