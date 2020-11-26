5c5a1304e6a647956dda1e17f27de48d553ebdd4
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-26 09:48:52 +0900

    [refactor] 1. 채널 누르면 색깔 바뀌게 구현 2. 커스텀 훅으로 state를 관리

--- client/src/components/ChannelListBox/ChannelList/Channel/Channel.tsx
+++ client/src/components/ChannelListBox/ChannelList/Channel/Channel.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 import { RootState } from '@/store/modules';
-import React, { useCallback, useState } from 'react';
+import React, { Component, ReactChild, useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { onChangeCurrent } from '@/store/modules/channels';
 import styled from 'styled-components';
+import { useChannelList, useChannels } from '@/hooks/useChannels';
 
 interface Args {
   key: number;
   idx: number;
 }
 
-const ChannelWrapper = styled.div`
+interface Props {
+  background: boolean;
+}
+
+const ChannelWrapper = styled.div<Props>`
   display: flex;
   align-items: center;
   margin-bottom: 10px;
@@ -21,6 +26,7 @@ const ChannelWrapper = styled.div`
   &: hover {
     background: rgba(0, 0, 0, 0.2);
   }
+  background: ${(props: Props) => (props.background ? 'blue' : 'transparent')};
 `;
 
 const IconWrapper = styled.div`
@@ -31,19 +37,15 @@ const NameWrapper = styled.div``;
 
 const Channel = (args: Args) => {
   const dispatch = useDispatch();
-  const { id, name, isPublic, current } = useSelector((state: RootState) => ({
-    id: state.channels.channelList[args.idx].id,
-    name: state.channels.channelList[args.idx].name,
-    isPublic: state.channels.channelList[args.idx].isPublic,
-    current: state.channels.current,
-  }));
+  const { id, name, isPublic } = useChannelList(args.idx);
+  const { current } = useChannels();
 
   const onClick = useCallback(() => {
     dispatch(onChangeCurrent(id));
   }, []);
 
   return (
-    <ChannelWrapper onClick={onClick} role="button">
+    <ChannelWrapper onClick={onClick} role="button" background={id === current}>
       <IconWrapper>{isPublic === 1 ? '#' : 'O'}</IconWrapper>
       <NameWrapper>{name}</NameWrapper>
     </ChannelWrapper>
