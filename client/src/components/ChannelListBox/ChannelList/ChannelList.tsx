19d3fb52ab6e4a344d0aff396c81c3688184af53
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-26 09:46:35 +0900

    [refactor] 1. 커스텀 훅으로 state를 가져옴 2. 쓸데없이 <div>로 감싸던 것을 <>를 이용하여 감쌈 3. useCallback이 불필요하게 사용되어 삭제

--- client/src/components/ChannelListBox/ChannelList/ChannelList.tsx
+++ client/src/components/ChannelListBox/ChannelList/ChannelList.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadChannelsRequest } from '@/store/modules/channels';
 import { RootState } from '@/store/modules';
+import { useChannels } from '@/hooks/useChannels';
 import Channel from './Channel/Channel';
 
 interface ChannelItem {
@@ -18,36 +19,28 @@ interface ChannelItem {
 
 const ChannelList = () => {
   const dispatch = useDispatch();
-  const { channelList, showList, current } = useSelector((state: RootState) => ({
-    channelList: state.channels.channelList,
-    showList: state.channels.showList,
-    current: state.channels.current,
-  }));
+  const { channelList, showList, current } = useChannels();
 
-  const callAPI = useCallback(() => {
-    dispatch(loadChannelsRequest());
-  }, []);
+  const callAPI = () => dispatch(loadChannelsRequest());
 
   useEffect(() => {
     callAPI();
   }, []);
 
   return (
-    <div>
-      {channelList?.map(
-        (channel: ChannelItem, idx: number) =>
-          !showList ? (
-            current === channel.id ? (
-              <Channel idx={idx} key={channel.id} />
-            ) : (
-              ''
-            )
-          ) : (
+    <>
+      {channelList?.map((channel: ChannelItem, idx: number) =>
+        !showList ? (
+          current === channel.id ? (
             <Channel idx={idx} key={channel.id} />
-          ),
-        // <Channel idx={idx} key={channel.id} />
+          ) : (
+            ''
+          )
+        ) : (
+          <Channel idx={idx} key={channel.id} />
+        ),
       )}
-    </div>
+    </>
   );
 };
 
