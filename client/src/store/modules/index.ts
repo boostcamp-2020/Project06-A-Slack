6330eccfe80f526fcbf5c718f21758fa5780a1e4
rnjshippo <tkstka0023@naver.com>
2020-11-27 00:24:30 +0900

    [feat] user sage 및 reducer 생성

--- client/src/store/modules/index.ts
+++ client/src/store/modules/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import channelSlice, { CHANNELS } from './channels';
 import authSlice, { AUTH } from './auth';
 import threadSlice, { THREAD } from './thread';
+import userSlice, { USER } from './user';
 
 const rootReducer = combineReducers({
   [CHANNELS]: channelSlice,
   [AUTH]: authSlice,
   [THREAD]: threadSlice,
+  [USER]: userSlice,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
