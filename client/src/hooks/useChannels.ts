9a7884e1c68c88ecf2ccc570f4ede1de986febb0
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-26 09:44:37 +0900

    [feat] channel에서 state를 가져오는 부분을 훅으로 제작

---
+++ client/src/hooks/useChannels.ts
@@ -0,0 +1,19 @@
+import { useSelector } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '@/store/modules';
+import { create } from 'domain';
+
+const selectChannelsState = (state: RootState) => state.channels;
+const selectChannels = createSelector(selectChannelsState, (channels) => channels);
+
+const selectChannelListState = (state: RootState) => state.channels.channelList;
+const selectChannelList = (idx: number) =>
+  createSelector(selectChannelListState, (channelList) => channelList[idx]);
+
+export const useChannels = () => {
+  return useSelector(selectChannels);
+};
+
+export const useChannelList = (idx: number) => {
+  return useSelector(selectChannelList(idx));
+};
