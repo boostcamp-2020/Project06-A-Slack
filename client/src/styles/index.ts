7f1246efa3ae0498fd8b67b16eb68bbe50dca631
rnjshippo <tkstka0023@naver.com>
2020-11-26 21:36:32 +0900

    [feat] 공통 스타일 정의

--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -3,4 +3,10 @@ import { normalize } from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
+  input, button {
+    border: none;
+    box-sizing: border-box;
+    font-size: 1rem;
+  }
+
 `;
