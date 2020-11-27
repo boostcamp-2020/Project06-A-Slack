b8fb89456279651b3a5c40bccc57e8f31081a630
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-27 09:46:20 +0900

    [feat] 특정 채널 상세 내용을 보여주는 DetailBox를 추가(미완성)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '@/hooks';
 import { useDispatch } from 'react-redux';
 import { logoutRequest } from '@/store/modules/auth';
-import { ChannelListBox, ThreadListBox } from '@/components';
+import { ChannelListBox, ThreadListBox, DetailBox } from '@/components';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -24,6 +24,7 @@ const Home = () => {
             Logout
           </button>
           <ThreadListBox />
+          <DetailBox />
         </>
       ) : (
         <Link to="/login">
