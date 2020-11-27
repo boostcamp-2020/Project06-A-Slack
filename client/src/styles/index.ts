28c1855f4e0265c0749abe055a0a0c310eda804f
rnjshippo <tkstka0023@naver.com>
2020-11-27 11:01:48 +0900

    [feat] 공통 스타일 추가 정의

--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -1,16 +1,38 @@
-import { createGlobalStyle } from 'styled-components';
+import { css, createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
-export const GlobalStyle = createGlobalStyle`
-  ${normalize}
+const commonStyle = css`
+  html {
+    height: 100%;
+  }
 
-  *, *:before, *:after {
+  body {
+    position: relative;
+    height: 100%;
+  }
+
+  #root {
+    position: relative;
+    height: 100%;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+
+  *,
+  *:before,
+  *:after {
     box-sizing: border-box;
   }
-  
-  input, button {
+
+  input,
+  button {
     border: none;
     font-size: 1rem;
   }
+`;
 
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  ${commonStyle}
 `;
