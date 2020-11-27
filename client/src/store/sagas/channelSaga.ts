9ebc46f662bac481bcb5a07f80de0a547c1f18b8
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-27 09:53:20 +0900

    [feat] 특정 채널에 속한 유저를 불러오는 비동기 api 통신을 위한 사가를 추가

--- client/src/store/sagas/channelSaga.ts
+++ client/src/store/sagas/channelSaga.ts
@@ -1,9 +1,20 @@
-import { all, fork, takeEvery, call, put } from 'redux-saga/effects';
-import { channelsService } from '@/services/channels.service';
-import { loadChannelsRequest, loadChannelsSuccess, loadChannelsFailure } from '../modules/channels';
+import { all, fork, takeEvery, call, put, takeLatest, take } from 'redux-saga/effects';
+import { channelService } from '@/services/channel.service';
+import {
+  loadChannelsRequest,
+  loadChannelsSuccess,
+  loadChannelsFailure,
+  loadChannelRequest,
+  loadChannelSuccess,
+  loadChannelFailure,
+} from '../modules/channel';
 
 function loadChannelsAPI() {
-  return channelsService.getChannels();
+  return channelService.getChannels();
+}
+
+function loadChannelAPI(channelId: number) {
+  return channelService.getChannel({ channelId });
 }
 
 function* loadChannels() {
@@ -15,10 +26,25 @@ function* loadChannels() {
   }
 }
 
+function* loadChannel(action: any) {
+  console.log(action);
+  try {
+    const result = yield call(loadChannelAPI, action.payload);
+    console.log(result);
+    yield put(loadChannelSuccess({ channel: result.data.channel, users: result.data.users }));
+  } catch (err) {
+    yield put(loadChannelFailure(err));
+  }
+}
+
 function* watchLoadChannels() {
   yield takeEvery(loadChannelsRequest, loadChannels);
 }
 
+function* watchLoadChannel() {
+  yield takeLatest(loadChannelRequest, loadChannel);
+}
+
 export default function* channelSaga() {
-  yield all([fork(watchLoadChannels)]);
+  yield all([fork(watchLoadChannels), fork(watchLoadChannel)]);
 }
