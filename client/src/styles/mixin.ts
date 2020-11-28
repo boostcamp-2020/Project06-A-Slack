025e724007f9161da62d2303c29cca45e040769f
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-28 21:37:21 +0900

    [feat] mixin의 flex에서 flex 정렬 방향을 추가함

--- client/src/styles/mixin.ts
+++ client/src/styles/mixin.ts
@@ -1,7 +1,8 @@
 import styled, { css } from 'styled-components';
 
-export const flex = (alignItems?: string, justifyContent?: string) => css`
+export const flex = (alignItems?: string, justifyContent?: string, flexDirection?: string) => css`
   align-items: ${alignItems ?? 'center'};
   justify-content: ${justifyContent ?? 'center'};
   display: flex;
+  flex-direction: ${flexDirection ?? 'row'};
 `;
