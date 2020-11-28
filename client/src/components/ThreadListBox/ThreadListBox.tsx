8e695fca0ed206805324eb2ca0abc03ba070a70d
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-28 21:39:07 +0900

    [refactor] 필요없는 부분 삭제하고 너비를 화면 전체로 줌

--- client/src/components/ThreadListBox/ThreadListBox.tsx
+++ client/src/components/ThreadListBox/ThreadListBox.tsx
@@ -6,14 +6,10 @@ import ThreadInputBox from './ThreadInputBox/ThreadInputBox';
 
 const StyledThreadListBox = styled.div`
   background-color: pink;
+  width: 100%;
 `;
 
 const ThreadListBox = () => {
-  const channel = {
-    id: 1,
-    name: 'slack-클론-구현팀',
-  };
-
   return (
     <StyledThreadListBox>
       <div>BoxTop</div>
