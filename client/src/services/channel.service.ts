3685dbe7aab565f9180ed8df39d16242c7da44da
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-28 19:00:18 +0900

    [refactor] 인자 하나 전달하는데 인터페이스로 타입을 정해서 사용하는 것이 비효율적이라고 생각하여 삭제

--- client/src/services/channel.service.ts
+++ client/src/services/channel.service.ts
@@ -1,14 +1,10 @@
 import API from '@/api';
 
-interface Args {
-  channelId: number;
-}
-
 export const channelService = {
   getChannels() {
     return API.get('/api/channels');
   },
-  getChannel({ channelId }: Args) {
+  getChannel({ channelId }: { channelId: number }) {
     return API.get(`/api/channels/${channelId}`);
   },
 };
