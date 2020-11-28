15818b21ad61f908f564235896c26e4d1da25204
grap3fruit <swkwon333@gmail.com>
2020-11-28 15:00:36 +0900

    [feat] reply 옆에 3명 정보 보이는것 추가

--- client/src/components/common/ThreadItem/ThreadItem.tsx
+++ client/src/components/common/ThreadItem/ThreadItem.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getSubThreadRequest } from '@/store/modules/subThread';
 import { Thread } from '@/types';
 import styled from 'styled-components';
+import { useUser } from '@/hooks/useUser';
 
 interface ThreadItemProps {
   thread: Thread;
@@ -17,10 +18,20 @@ const Container = styled.div`
 `;
 
 const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
+  const { userInfo } = useSelector(useUser);
+
   const dispatch = useDispatch();
 
   const replyClickEventHandler = (e: any) => {
-    dispatch(getSubThreadRequest({ parentId: e.target.id }));
+    const parentId = e.target.closest('button').id;
+    dispatch(getSubThreadRequest({ parentId }));
+  };
+
+  const displaySubProfile = () => {
+    // subThreadId가 아니고 profile을 바로 가져와서 써버리자 그럼. id는 의미없어 이렇게 쓸거면 profile image로 바로.
+    // subThread craterId로
+    const results = [thread.subThreadId1, thread.subThreadId2, thread.subThreadId3];
+    return results;
   };
 
   return (
@@ -29,6 +40,9 @@ const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
       <div>{thread.createdAt}</div>
       <div>{thread.content}</div>
       <button id={String(thread.id)} type="button" onClick={replyClickEventHandler}>
+        {displaySubProfile()?.map((subProfile: number | null, index: number) => (
+          <div key={index}>{subProfile}</div>
+        ))}
         {thread.subCount}replies
       </button>
       <div>{thread.subThreadId1}</div>
