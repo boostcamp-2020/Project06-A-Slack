3275213197bbdabd8fb1676ce9a9a6991be60b9f
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-30 08:44:54 +0900

    [refactor] 네이밍 컨벤션에 맞게 가장 큰 요소는 Container로 수정

--- client/src/components/ChannelListBox/ChannelListBox.tsx
+++ client/src/components/ChannelListBox/ChannelListBox.tsx
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 import ChannelList from './ChannelList/ChannelList';
 import ChannelListHeader from './ChannelListHeader/ChannelListHeader';
 
-const ChannelListBoxWrapper = styled.div`
+const Container = styled.div`
   padding: ${(props) => props.theme.size.s} 0;
 `;
 
 const ChannelListBox = () => {
   return (
-    <ChannelListBoxWrapper>
+    <Container>
       <ChannelListHeader />
       <ChannelList />
-    </ChannelListBoxWrapper>
+    </Container>
   );
 };
 
