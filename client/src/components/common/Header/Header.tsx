d64fe156204905f829644e6a425740d88703abf5
rnjshippo <tkstka0023@naver.com>
2020-11-30 23:57:33 +0900

    [feat] 로그아웃 버튼 위치 이동

--- client/src/components/common/Header/Header.tsx
+++ client/src/components/common/Header/Header.tsx
@@ -4,8 +4,10 @@ import { useDispatch } from 'react-redux';
 import { getUserRequest } from '@/store/modules/user';
 import { flex } from '@/styles/mixin';
 import { useAuth } from '@/hooks';
+import { logoutRequest } from '@/store/modules/auth';
 
 const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 2.5rem;
   background-color: ${(props) => props.theme.color.main};
@@ -17,13 +19,23 @@ const Title = styled.div`
   font-size: 0.8rem;
   color: ${(props) => props.theme.color.gray5};
   ${flex()};
+  user-select: none;
 `;
 
 const ProfileImg = styled.img``;
 
+const LogoutButton = styled.button`
+  position: absolute;
+  right: 0.5rem;
+`;
+
 const Header = () => {
   const { userId } = useAuth();
   const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutRequest());
+  };
 
   useEffect(() => {
     if (userId) {
@@ -34,6 +46,7 @@ const Header = () => {
   return (
     <Container>
       <Title>부스트캠프 2020 멤버십</Title>
+      <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
     </Container>
   );
 };
