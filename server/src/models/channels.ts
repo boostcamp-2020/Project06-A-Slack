ead7f07d18d7ad76165447c2cb6383527148bb9b
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-30 23:11:00 +0900

    [feat] 채널 추가하는 로직을 서버에서 구현

--- server/src/models/channels.ts
+++ server/src/models/channels.ts
@@ -1,6 +1,19 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import pool from '@/db';
 
+interface createInfo {
+  ownerId: number;
+  name: string;
+  channelType: number;
+  isPublic: number;
+  description: string;
+}
+
+interface JoinInfo {
+  userId: number;
+  channelId: number;
+}
+
 export const channelModel = {
   getChannels() {
     const sql = `SELECT channel_type as channelType, id, description, is_public as isPublic, 
@@ -12,7 +25,7 @@ export const channelModel = {
     member_count as memeberCount, name, owner_id as ownerId, topic
     FROM channel 
     JOIN user_channel
-    ON user_channel.user_id = ${userId} AND user_channel.channel_id = channel.id
+    ON user_channel.user_id = ? AND user_channel.channel_id = channel.id
     WHERE channel.is_deleted = 0
     `;
     return pool.execute(sql, [userId]);
@@ -22,7 +35,7 @@ export const channelModel = {
     user.display_name as displayName
     FROM user
     JOIN user_channel
-    ON user_channel.channel_id = ${channelId} and user_channel.user_id = user.id
+    ON user_channel.channel_id = ? and user_channel.user_id = user.id
     `;
     return pool.execute(sql, [channelId]);
   },
@@ -30,8 +43,16 @@ export const channelModel = {
     const sql = `SELECT id, owner_id as ownerId, name, channel_type as channelType, is_public as isPublic, 
     is_deleted as isDeleted, member_count as memberCount, description
     FROM channel
-    WHERE channel.id = ${channelId}
+    WHERE channel.id = ?
     `;
     return pool.execute(sql, [channelId]);
+  },
+  createChannel({ ownerId, name, channelType, isPublic, description }: createInfo): any {
+    const sql = `INSERT INTO channel (owner_id, name, channel_type, is_public, description) VALUES (?, ?, ?, ?, ?)`;
+    return pool.execute(sql, [ownerId, name, channelType, isPublic, description]);
+  },
+  joinChannel({ userId, channelId }: JoinInfo): any {
+    const sql = `INSERT INTO user_channel (user_id, channel_id) VALUES(?, ?)`;
+    return pool.execute(sql, [userId, channelId]);
   },
 };
