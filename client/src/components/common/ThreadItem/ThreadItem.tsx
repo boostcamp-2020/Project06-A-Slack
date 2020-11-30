bf8f58b00ac834ba944c80cc911c8571c7ccc0a8
grap3fruit <swkwon333@gmail.com>
2020-11-30 22:04:21 +0900

    [feat] 쓰레드에 reply 0개인 경우 안뜨도록 변경

--- client/src/components/common/ThreadItem/ThreadItem.tsx
+++ client/src/components/common/ThreadItem/ThreadItem.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSubThreadRequest } from '@/store/modules/subThread';
 import { Thread } from '@/types';
-import { useUser } from '@/hooks/useUser';
+import { useUser } from '@/hooks';
 import { flex } from '@/styles/mixin';
+import { getNotNullDataInArray } from '@/utils/utils';
 
 const Container = styled.div`
   background-color: white;
@@ -20,32 +21,20 @@ const Button = styled.button`
 
 interface ThreadItemProps {
   thread: Thread;
+  isParentThreadOfRightSideBar: boolean;
 }
 
-interface ReplyData {
-  subThreadUserId: number | null;
-  subThreadProfile: number | null;
-}
-
-const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
-  const { userInfo } = useSelector(useUser);
+const ThreadItem: React.FC<ThreadItemProps> = ({
+  thread,
+  isParentThreadOfRightSideBar,
+}: ThreadItemProps) => {
+  const { userInfo } = useUser();
   const buttonEl = useRef<HTMLButtonElement>(null);
-
   const dispatch = useDispatch();
 
-  const replyClickEventHandler = () => {
+  const replyClickEventHandler = (clickedThread: any) => {
     const parentId = Number(buttonEl.current?.id);
-    dispatch(getSubThreadRequest({ parentId }));
-  };
-
-  const getFilteredReplyData = (replyData: ReplyData[]) => {
-    const results = replyData.filter(({ subThreadUserId }) => {
-      if (subThreadUserId === null) {
-        return false;
-      }
-      return true;
-    });
-    return results;
+    dispatch(getSubThreadRequest({ parentId, parentThread: clickedThread }));
   };
 
   const getDisplayReplyData = () => {
@@ -55,22 +44,32 @@ const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
       { subThreadUserId: thread.subThreadUserId2, subThreadProfile: thread.subThreadUserId2 },
       { subThreadUserId: thread.subThreadUserId3, subThreadProfile: thread.subThreadUserId3 },
     ];
-    const results = getFilteredReplyData(replyData);
+    const results = getNotNullDataInArray(replyData)('subThreadUserId');
     return results;
   };
 
   return (
     <Container>
+      {thread.parentId}
       <div>{thread.userId}</div>
       <div>{thread.createdAt}</div>
       <div>{thread.content}</div>
-      <Button ref={buttonEl} id={String(thread.id)} type="button" onClick={replyClickEventHandler}>
-        {getDisplayReplyData().map((el) => {
-          const { subThreadUserId, subThreadProfile } = el;
-          return <div key={`${thread.id}${subThreadUserId}`}>{subThreadProfile}</div>;
-        })}
-        {thread.subCount}replies
-      </Button>
+      {thread.subCount === 0 || isParentThreadOfRightSideBar ? (
+        ''
+      ) : (
+        <Button
+          ref={buttonEl}
+          id={String(thread.id)}
+          type="button"
+          onClick={() => replyClickEventHandler(thread)}
+        >
+          {getDisplayReplyData().map((el) => {
+            const { subThreadUserId, subThreadProfile } = el;
+            return <div key={`${thread.id}${subThreadUserId}`}>{subThreadProfile}</div>;
+          })}
+          {thread.subCount}replies
+        </Button>
+      )}
     </Container>
   );
 };
