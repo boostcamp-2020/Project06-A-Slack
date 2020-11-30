9275ddad10aeca64f7ed85b2a371d81c40275f7b
grap3fruit <swkwon333@gmail.com>
2020-11-30 22:01:26 +0900

    [feat] 현재 채널에 따라 쓰레드 목록 보여주도록 구현

--- client/src/components/ThreadListBox/ThreadList/ThreadList.tsx
+++ client/src/components/ThreadListBox/ThreadList/ThreadList.tsx
@@ -4,26 +4,26 @@ import { getThreadRequest } from '@/store/modules/thread';
 import styled from 'styled-components';
 import { Thread } from '@/types';
 import ThreadItem from '@/components/common/ThreadItem/ThreadItem';
-import { useThread } from '@/hooks';
+import { useThread, useChannel } from '@/hooks';
 
 const Container = styled.div`
   background-color: orange;
 `;
 
 const ThreadList = () => {
+  const { current } = useChannel();
   const { threadList } = useThread();
   const dispatch = useDispatch();
-  const channelId = 1;
 
   useEffect(() => {
-    dispatch(getThreadRequest({ channelId }));
-  }, [dispatch]);
+    dispatch(getThreadRequest({ channelId: Number(current?.id) }));
+  }, [dispatch, current]);
 
   return (
     <Container>
-      <div>ThreadListTop</div>
+      <div>ThreadListTop{current?.id}</div>
       {threadList?.map((thread: Thread) => (
-        <ThreadItem key={thread.id} thread={thread} />
+        <ThreadItem key={thread.id} thread={thread} isParentThreadOfRightSideBar={false} />
       ))}
     </Container>
   );
