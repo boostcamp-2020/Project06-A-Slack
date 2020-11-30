9262c2a29a7ff53e927a2c3c1ee3de1784deb60e
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-30 18:24:33 +0900

    [feat] 계정에 따른 채널을 불러오도록 server 로직으 구현

--- server/src/models/channels.ts
+++ server/src/models/channels.ts
@@ -1,14 +1,21 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import pool from '@/db';
-
-interface Args {
-  channelId?: number;
-}
 
 export const channelModel = {
   getChannels() {
     const sql = `SELECT channel_type as channelType, id, description, is_public as isPublic, 
-    member_count as memeberCount, name, owner_id as ownerId FROM channel where is_deleted = 0`;
+    member_count as memeberCount, name, owner_id as ownerId, topic FROM channel WHERE is_deleted = 0 AND is_public=1`;
     return pool.query(sql);
+  },
+  getJoinChannels({ userId }: { userId: number }): any {
+    const sql = `SELECT channel_type as channelType, id, description, is_public as isPublic, 
+    member_count as memeberCount, name, owner_id as ownerId, topic
+    FROM channel 
+    JOIN user_channel
+    ON user_channel.user_id = ${userId} AND user_channel.channel_id = channel.id
+    WHERE channel.is_deleted = 0
+    `;
+    return pool.execute(sql, [userId]);
   },
   getChannelUser({ channelId }: { channelId: number }): any {
     const sql = `SELECT user_channel.user_id as userId, 
--- server/src/routes/api/users/[userId]/index.ts
+++ server/src/routes/api/users/[userId]/index.ts
@@ -7,5 +7,6 @@ router.get('/', userIdController.getUser);
 router.post('/', userIdController.modifyUser);
 router.delete('/', userIdController.deleteUser);
 router.post('/last-channel', userIdController.modifyLastChannel);
+router.get('/channels', userIdController.getJoinChannels);
 
 export default router;
