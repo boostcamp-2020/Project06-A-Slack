9f7307273e87aeee654748b9741e0475e3a34ed5
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-30 09:14:03 +0900

    [refactor] 네이밍 컨벤션에 맞게 제일 큰 컴포넌트를 Container로 수정

--- client/src/components/DetailBox/DetailBox.tsx
+++ client/src/components/DetailBox/DetailBox.tsx
@@ -5,7 +5,7 @@ import DetailHeader from './DeatailHeader/DetailHeader';
 import DetailButtonBox from './DetailButtonBox/DetailButtonBox';
 import DetailList from './DetailList/DetailList';
 
-const StyledDetailBox = styled.div`
+const Container = styled.div`
   width: 600px;
   display: ${(props: { visible: boolean }) => (props.visible ? 'block' : 'none')};
 `;
@@ -13,11 +13,11 @@ const DetailBox = () => {
   const { detailVisible } = useChannel();
 
   return (
-    <StyledDetailBox visible={detailVisible}>
+    <Container visible={detailVisible}>
       <DetailHeader />
       <DetailButtonBox />
       <DetailList />
-    </StyledDetailBox>
+    </Container>
   );
 };
 
