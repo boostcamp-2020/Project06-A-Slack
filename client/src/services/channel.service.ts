7a2dafb2be13a3212e346fcb702ef70d63438c86
hansanguk0222 <hansanguk0222@gmail.com>
2020-11-30 18:23:50 +0900

    [feat] 계정에 따른 채널을 불러오도록 api를 설계

--- client/src/services/channel.service.ts
+++ client/src/services/channel.service.ts
@@ -4,6 +4,9 @@ export const channelService = {
   getChannels() {
     return API.get('/api/channels');
   },
+  getJoinChannels({ userId }: { userId: number }) {
+    return API.get(`/api/users/${userId}/channels`);
+  },
   getChannel({ channelId }: { channelId: number }) {
     return API.get(`/api/channels/${channelId}`);
   },
--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,2 +1,3 @@
 export { authService } from './auth.service';
 export { userService } from './user.service';
+export { channelService } from './channel.service';
