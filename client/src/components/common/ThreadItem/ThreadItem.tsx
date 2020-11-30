bf0b6e605688734862881547b1d43cf73a841019
grap3fruit <swkwon333@gmail.com>
2020-11-30 18:01:55 +0900

    [refactor] threaditem reply 뷰 로직 변경
    key값에 null로 들어가는 경우에 발생되는 키중복 cas를 제거하기 위해 filter 로직 추가.
    filter 함수(getFilteredReplyData) 구현해서 get 함수에서 호출 후 리턴.

--- client/src/components/common/ThreadItem/ThreadItem.tsx
+++ client/src/components/common/ThreadItem/ThreadItem.tsx
@@ -6,10 +6,6 @@ import { Thread } from '@/types';
 import { useUser } from '@/hooks/useUser';
 import { flex } from '@/styles/mixin';
 
-interface ThreadItemProps {
-  thread: Thread;
-}
-
 const Container = styled.div`
   background-color: white;
   border: 1px solid black;
@@ -22,6 +18,15 @@ const Button = styled.button`
   ${flex()};
 `;
 
+interface ThreadItemProps {
+  thread: Thread;
+}
+
+interface ReplyData {
+  subThreadUserId: number | null;
+  subThreadProfile: number | null;
+}
+
 const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
   const { userInfo } = useSelector(useUser);
   const buttonEl = useRef<HTMLButtonElement>(null);
@@ -33,9 +38,24 @@ const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
     dispatch(getSubThreadRequest({ parentId }));
   };
 
-  const displaySubProfile = () => {
+  const getFilteredReplyData = (replyData: ReplyData[]) => {
+    const results = replyData.filter(({ subThreadUserId }) => {
+      if (subThreadUserId === null) {
+        return false;
+      }
+      return true;
+    });
+    return results;
+  };
+
+  const getDisplayReplyData = () => {
     // 추후 subThreadUserId 값을 이용해, 채널 멤버의 user 정보를 활용하여 프로필을 뿌려준다.
-    const results = [thread.subThreadUserId1, thread.subThreadUserId2, thread.subThreadUserId3];
+    const replyData = [
+      { subThreadUserId: thread.subThreadUserId1, subThreadProfile: thread.subThreadUserId1 },
+      { subThreadUserId: thread.subThreadUserId2, subThreadProfile: thread.subThreadUserId2 },
+      { subThreadUserId: thread.subThreadUserId3, subThreadProfile: thread.subThreadUserId3 },
+    ];
+    const results = getFilteredReplyData(replyData);
     return results;
   };
 
@@ -45,9 +65,10 @@ const ThreadItem: React.FC<ThreadItemProps> = ({ thread }: ThreadItemProps) => {
       <div>{thread.createdAt}</div>
       <div>{thread.content}</div>
       <Button ref={buttonEl} id={String(thread.id)} type="button" onClick={replyClickEventHandler}>
-        {displaySubProfile()?.map((subProfile: number | null, index: number) => (
-          <div key={index}>{subProfile}</div>
-        ))}
+        {getDisplayReplyData().map((el) => {
+          const { subThreadUserId, subThreadProfile } = el;
+          return <div key={`${thread.id}${subThreadUserId}`}>{subThreadProfile}</div>;
+        })}
         {thread.subCount}replies
       </Button>
     </Container>
