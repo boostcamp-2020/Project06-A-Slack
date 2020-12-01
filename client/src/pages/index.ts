001b8c0a276d0037e5d3e1b802c5b99e76416d21
grap3fruit <swkwon333@gmail.com>
2020-12-01 15:57:36 +0900

    [refactor] App에서 WorkSpacePage 분리

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as LoginPage } from './LoginPage';
 export { default as HomePage } from './HomePage';
 export { default as EmailVerifyPage } from './EmailVerifyPage';
 export { default as SignupPage } from './SignupPage';
+export { default as WorkSpacePage } from './WorkSpacePage';
