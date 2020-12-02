415f2f2992e43db53bf90a9284a60f66d19f6de0
grap3fruit <swkwon333@gmail.com>
2020-12-02 14:12:50 +0900

    [refactor] 필요없는 코드 제거

--- client/src/components/common/ThreadItem/ReplyButton/ReplyButton.tsx
+++ client/src/components/common/ThreadItem/ReplyButton/ReplyButton.tsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
-import { getSubThreadRequest } from '@/store/modules/subThread';
-import { Thread } from '@/types';
-import { useChannel } from '@/hooks';
+import { Thread, JoinUser } from '@/types';
 import { flex } from '@/styles/mixin';
 import { getNotNullDataInArray } from '@/utils/utils';
 import { Link } from 'react-router-dom';
+import { USER_DEFAULT_PROFILE_URL } from '@/utils/constants';
+import { useChannel } from '@/hooks/useChannel';
 
-const Button = styled.button`
-  ${flex()};
+const Reply = styled.button`
+  ${flex('row', 'flext-start')};
+`;
+
+const Profile = styled.img`
+  height: ${(props) => props.theme.size.xxxl};
+  width: ${(props) => props.theme.size.xxxl};
 `;
 
 interface ReplyButtonProps {
@@ -17,23 +21,14 @@ interface ReplyButtonProps {
 }
 
 const ReplyButton: React.FC<ReplyButtonProps> = ({ thread }: ReplyButtonProps) => {
-  const buttonEl = useRef<HTMLButtonElement>(null);
-  const dispatch = useDispatch();
-
-  const replyClickEventHandler = (
-    clickedThread: Thread,
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    const parentId = Number(e.currentTarget.id);
-    dispatch(getSubThreadRequest({ parentId, parentThread: clickedThread }));
-  };
+  const { users } = useChannel();
 
   const getDisplayReplyData = () => {
     // 추후 subThreadUserId 값을 이용해, 채널 멤버의 user 정보를 활용하여 프로필을 뿌려준다.
     const replyData = [
-      { subThreadUserId: thread.subThreadUserId1, subThreadProfile: thread.subThreadUserId1 },
-      { subThreadUserId: thread.subThreadUserId2, subThreadProfile: thread.subThreadUserId2 },
-      { subThreadUserId: thread.subThreadUserId3, subThreadProfile: thread.subThreadUserId3 },
+      { subThreadUserId: thread.subThreadUserId1, subThreadProfile: USER_DEFAULT_PROFILE_URL },
+      { subThreadUserId: thread.subThreadUserId2, subThreadProfile: USER_DEFAULT_PROFILE_URL },
+      { subThreadUserId: thread.subThreadUserId3, subThreadProfile: USER_DEFAULT_PROFILE_URL },
     ];
     const results = getNotNullDataInArray(replyData)('subThreadUserId');
     return results;
@@ -41,18 +36,19 @@ const ReplyButton: React.FC<ReplyButtonProps> = ({ thread }: ReplyButtonProps) =
 
   return (
     <Link to={`/client/1/${thread.channelId}/thread/${thread.id}`}>
-      <Button
-        ref={buttonEl}
-        id={String(thread.id)}
-        type="button"
-        onClick={(e) => replyClickEventHandler(thread, e)}
-      >
+      <Reply id={String(thread.id)}>
         {getDisplayReplyData().map((el) => {
           const { subThreadUserId, subThreadProfile } = el;
-          return <div key={`${thread.id}${subThreadUserId}`}>{subThreadProfile}</div>;
+          return (
+            <Profile
+              src={subThreadProfile === null ? USER_DEFAULT_PROFILE_URL : subThreadProfile}
+              key={`${thread.id}${subThreadUserId}`}
+              alt="subThreadUserImage"
+            />
+          );
         })}
         {thread.subCount}replies
-      </Button>
+      </Reply>
     </Link>
   );
 };
