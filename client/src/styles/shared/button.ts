0d6be44404dc0bf227d87366afc0fae3d257c955
rnjshippo <tkstka0023@naver.com>
2020-12-02 18:21:52 +0900

    [feat] 공통 버튼 disabled 상태의 css 수정

--- client/src/styles/shared/button.ts
+++ client/src/styles/shared/button.ts
@@ -17,11 +17,14 @@ export const SubmitButton = styled(Button)`
   color: white;
   background-color: ${(props) => props.theme.color.green1};
   border: 1px solid ${(props) => lighten(0.03, props.theme.color.green1)};
-  &:hover {
+  &:hover:not([disabled]) {
     background-color: ${(props) => lighten(0.03, props.theme.color.green1)};
   }
-  &:active {
+  &:active:not([disabled]) {
     background-color: ${(props) => lighten(0.06, props.theme.color.green1)};
+  }
+  &:disabled {
+    cursor: wait;
   }
 `;
 
@@ -29,10 +32,13 @@ export const CancelButton = styled(Button)`
   color: ${(props) => props.theme.color.black3};
   background-color: ${(props) => props.theme.color.semiWhite};
   border: 1px solid ${(props) => props.theme.color.lightGray1};
-  &:hover {
+  &:hover:not([disabled]) {
     background-color: ${(props) => darken(0.02, props.theme.color.semiWhite)};
   }
-  &:active {
+  &:active:not([disabled]) {
     background-color: ${(props) => darken(0.04, props.theme.color.semiWhite)};
+  }
+  &:disabled {
+    cursor: wait;
   }
 `;
