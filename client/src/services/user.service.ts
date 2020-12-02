f0a8c60bec55a41f8211c23abb738aafa0f48b2a
rnjshippo <tkstka0023@naver.com>
2020-12-02 21:44:20 +0900

    [feat] user.service 코드 수정 - formData 타입으로 변경

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,16 +1,12 @@
 import API from '@/api';
 
-interface EditUserParams {
-  id: number;
-  displayName: string;
-  phoneNumber: string;
-}
-
 export const userService = {
   getUser({ id }: { id: number }): any {
     return API.get(`/api/users/${id}`);
   },
-  editUser({ id, displayName, phoneNumber }: EditUserParams): any {
-    return API.post(`/api/users/${id}`, { id, displayName, phoneNumber });
+  editUser({ id, formData }: { id: number; formData: FormData }): any {
+    return API.post(`/api/users/${id}`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
   },
 };
