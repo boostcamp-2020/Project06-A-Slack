914e9a4dde657724b537cb49bc8a009153da6fed
rnjshippo <tkstka0023@naver.com>
2020-12-02 18:23:11 +0900

    [fix] userService editUser의 body 값 전달 안한부분 수정

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,10 +1,16 @@
 import API from '@/api';
 
+interface EditUserParams {
+  id: number;
+  displayName: string;
+  phoneNumber: string;
+}
+
 export const userService = {
   getUser({ id }: { id: number }): any {
     return API.get(`/api/users/${id}`);
   },
-  editUser({ id }: { id: number; displayName: string; phoneNumber: string }): any {
-    return API.post(`/api/users/${id}`);
+  editUser({ id, displayName, phoneNumber }: EditUserParams): any {
+    return API.post(`/api/users/${id}`, { id, displayName, phoneNumber });
   },
 };
