eb9265d63072e167c1fec6e642237c2a938ea3a3
rnjshippo <tkstka0023@naver.com>
2020-12-02 21:47:29 +0900

    [refactor] dim-modal 컴포넌트 수정으로 인한 파일 삭제

--- client/src/components/common/Header/UserProfileModalBox/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as UserProfileModalHeader } from './UserProfileHeader/UserProfileHeader';
-export { default as UserProfileModalBody } from './UserProfileBody/UserProfileBody';
-export { default as UserProfileModalFooter } from './UserProfileFooter/UserProfileFooter';
