a455b2c538eb8ce4de7e1aa2b75d561890fe78d7
rnjshippo <tkstka0023@naver.com>
2020-12-03 23:10:15 +0900

    [feat] 404 대신에 home으로 redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import {
   NotFoundPage,
@@ -27,7 +27,7 @@ const App = () => {
             exact
             component={WorkSpacePage}
           />
-          <Route component={NotFoundPage} />
+          <Redirect to="/" />
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
