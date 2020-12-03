14b8efeb49929529e1a536f28e2280ad3c1ea7fd
hansanguk0222 <hansanguk0222@gmail.com>
2020-12-03 12:44:47 +0900

    [feat] 페이지 라우팅 구현 1. 채널 아이디 라우팅 예외 처리(가입 안된 채널, 문자열)를 워크 스페이스 페이지에 작성 2. 로그인 되면 마지막으로 봤던 페이지로 쏴주기

--- client/src/pages/WorkSpacePage.tsx
+++ client/src/pages/WorkSpacePage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
-import { useAuth } from '@/hooks';
+import { Redirect, useHistory, useParams } from 'react-router-dom';
+import { useAuth, useUser, useChannel } from '@/hooks';
 import { Header, LeftSideBar, ThreadListBox, RightSideBar } from '@/components';
+import { isExistedChannel, isNumberTypeValue } from '@/utils/utils';
+
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -18,6 +20,27 @@ interface RightSideParams {
 const WorkSpacePage: React.FC = () => {
   const { channelId, rightSideType }: RightSideParams = useParams();
   const { accessToken } = useAuth();
+  const { userInfo } = useUser();
+  const history = useHistory();
+  const { myChannelList } = useChannel();
+
+  // localshot:8080 접속시 연결될 채널을 안내
+  if (channelId === undefined && userInfo !== null) {
+    const url = `/client/1/${userInfo?.lastChannelId}`;
+    return <Redirect to={url} />;
+  }
+
+  // 채널에 접근 불가능한 URL이 들어온 경우
+  if (channelId !== undefined) {
+    if (!isNumberTypeValue(channelId)) {
+      // 숫자가 아닌 문자열이 들어오는 경우
+      history.goBack();
+    }
+    if (myChannelList.length !== 0 && !isExistedChannel({ channelId: +channelId, myChannelList })) {
+      // 가입 안 된 채널을 부르는 경우
+      history.goBack();
+    }
+  }
 
   return (
     <>
