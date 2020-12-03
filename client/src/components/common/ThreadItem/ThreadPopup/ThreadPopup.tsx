b3db92d0f74b6e71913c65740bbb716d42e00f95
grap3fruit <swkwon333@gmail.com>
2020-12-03 13:34:50 +0900

    [feat] popup에 moreaction 모달 추가

--- client/src/components/common/ThreadItem/ThreadPopup/ThreadPopup.tsx
+++ client/src/components/common/ThreadItem/ThreadPopup/ThreadPopup.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Thread } from '@/types';
 import { Link } from 'react-router-dom';
+import { DimModal, MenuModal } from '@/components';
 
 const Container = styled.div`
+  position: relative;
   background-color: blue;
   button {
     font-size: ${(props) => props.theme.size.xs};
+  }
+`;
+
+const Modal = styled.div`
+  position: absolute;
+  top: -0.5rem;
+  right: -8rem;
+`;
+
+const MoreActionButton = styled.button`
+  position: relative;
+`;
+
+const ModalListItem = styled.div`
+  width: 100%;
+  padding: 0.4rem 1.2rem;
+  font-size: 0.9rem;
+  cursor: pointer;
+  &:hover {
+    color: white;
+    background-color: ${(props) => props.theme.color.blue1};
   }
 `;
 
@@ -15,6 +38,15 @@ interface ThreadPopupProps {
 }
 
 const ThreadPopup: React.FC<ThreadPopupProps> = ({ thread }: ThreadPopupProps) => {
+  const [menuModalVisible, setMenuModalVisible] = useState(false);
+
+  const closeMenuModal = () => setMenuModalVisible(false);
+  const openMenuModal = () => setMenuModalVisible(true);
+
+  const openEditModal = () => {
+    console.log('click');
+  };
+
   return (
     <Container>
       <button type="button">reaction</button>
@@ -23,7 +55,20 @@ const ThreadPopup: React.FC<ThreadPopupProps> = ({ thread }: ThreadPopupProps) =
       </Link>
       <button type="button">shareMessage</button>
       <button type="button">Save</button>
-      <button type="button">MoreActions</button>
+      <MoreActionButton type="button" onClick={openMenuModal}>
+        MoreActions
+        {menuModalVisible && (
+          <Modal onClick={closeMenuModal}>
+            <MenuModal width="auto" visible={menuModalVisible} setVisible={setMenuModalVisible}>
+              <ModalListItem onClick={openEditModal}>Unfollow message</ModalListItem>
+              <ModalListItem>Copy link</ModalListItem>
+              <ModalListItem>Pin to this conversation</ModalListItem>
+              <ModalListItem>Edit message</ModalListItem>
+              <ModalListItem>Delete message</ModalListItem>
+            </MenuModal>
+          </Modal>
+        )}
+      </MoreActionButton>
     </Container>
   );
 };
