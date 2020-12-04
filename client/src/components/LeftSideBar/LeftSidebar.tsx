1c6c1dbb15a26fdfa4460ea691c8ae402c476d50
rnjshippo <tkstka0023@naver.com>
2020-12-04 20:35:25 +0900

    [feat] 왼쪽 헤더 최상단에 연필 아이콘 추가 및 폰트 두께 수정

--- client/src/components/LeftSideBar/LeftSidebar.tsx
+++ client/src/components/LeftSideBar/LeftSidebar.tsx
@@ -4,10 +4,9 @@ import LeftSidebarHeader from './LeftSideBarHeader/LeftSideBarHeader';
 import LeftSidebarContent from './LeftSideBarContent/LeftSideBarContent';
 
 const Container = styled.div`
+  width: 16.25rem;
   background: ${(props) => props.theme.color.purple2};
   height: 100%;
-  min-width: 150px;
-  width: 200px;
 `;
 
 const LeftSideBar = (): ReactElement => {
--- client/src/components/common/Icon/index.ts
+++ client/src/components/common/Icon/index.ts
@@ -1,3 +1,4 @@
 export { default as ClockIcon } from './ClockIcon/ClockIcon';
 export { default as UserStateIcon } from './UserStateIcon/UserStateIcon';
 export { default as WarningIcon } from './WarningIcon/WarningIcon';
+export { default as WriteIcon } from './WriteIcon/WriteIcon';
