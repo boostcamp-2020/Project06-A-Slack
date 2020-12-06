02114b2daa1ad8f375e90c00e8a0571c363f03f0
hansanguk0222 <hansanguk0222@gmail.com>
2020-12-06 21:22:27 +0900

    [feat] User 타입을 서버에 추가

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -3,4 +3,14 @@ export interface Error {
   message?: string;
 }
 
+export interface User {
+  id: number;
+  pw: string;
+  email: string;
+  displayName: string;
+  phoneNumber: string | null;
+  image: string;
+  lastChannelId: number | null;
+}
+
 export type AuthToken = 'ACCESS' | 'REFRESH';
