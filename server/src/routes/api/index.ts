aab23f06e6b5c6bc4c2ac4590ab7d7a81a26f857
grap3fruit <swkwon333@gmail.com>
2020-12-07 20:27:14 +0900

    [feat] emoji 관련 api 구현
    전체 이모지 리스트를 가져오는 getEmojiList,
    이모지를 추가하는 createEmoji
    두개의 api를 추가했습니다.

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -4,6 +4,7 @@ import userRouter from './users';
 import channelRouter from './channels';
 import authRouter from './auth';
 import threadRouter from './threads';
+import emojiRouter from './emojis';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/users', authenticated, userRouter);
 router.use('/channels', authenticated, channelRouter);
 router.use('/auth', authRouter);
 router.use('/threads', authenticated, threadRouter);
+router.use('/emojis', authenticated, emojiRouter);
 
 export default router;
