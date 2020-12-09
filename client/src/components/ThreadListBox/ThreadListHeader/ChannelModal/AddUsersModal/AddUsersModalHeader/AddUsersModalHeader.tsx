19c930451fc4623d9de3a3ddf079824dd8f4a2b9
rnjshippo <tkstka0023@naver.com>
2020-12-09 23:20:04 +0900

    [fix] 파일명 변경

---
+++ client/src/components/ThreadListBox/ThreadListHeader/ChannelModal/AddUsersModal/AddUsersModalHeader/AddUsersModalHeader.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { flex } from '@/styles/mixin';
+import { useChannelState } from '@/hooks';
+
+const Header = styled.div`
+  ${flex('center', 'flex-start', 'column')};
+`;
+
+const HeaderTitle = styled.div`
+  font-size: ${(props) => props.theme.size.l};
+`;
+
+const ChannelName = styled.div`
+  font-size: ${(props) => props.theme.size.s};
+`;
+
+interface AddUsersModalHeaderProps {
+  isDM: boolean;
+}
+
+const AddUserModalHeader: React.FC<AddUsersModalHeaderProps> = ({
+  isDM,
+}: AddUsersModalHeaderProps) => {
+  const { current } = useChannelState();
+  return (
+    <Header>
+      <HeaderTitle>Add People</HeaderTitle>
+      <ChannelName>
+        {!isDM && current?.isPublic}
+        {!isDM && current?.name}
+      </ChannelName>
+    </Header>
+  );
+};
+
+export default AddUserModalHeader;
