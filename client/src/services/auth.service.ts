83f7711de85bd44e41e38dd733444fc6f3808882
rnjshippo <tkstka0023@naver.com>
2020-12-11 14:06:14 +0900

    [feat] signup api 요청 service 구현

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -6,6 +6,12 @@ interface LoginParam {
   pw: string;
 }
 
+interface SignupParam {
+  email: string;
+  pw: string;
+  displayName: string;
+}
+
 export const authService: Service = {
   login({ email, pw }: LoginParam) {
     return API.post('/api/auth/login', { email, pw });
@@ -16,5 +22,8 @@ export const authService: Service = {
   },
   verifyEmail({ email }: { email: string }) {
     return API.post('/api/auth/email/verify', { email });
+  },
+  signup({ email, pw, displayName }: SignupParam) {
+    return API.post('/api/auth/signup', { email, pw, displayName });
   },
 };
